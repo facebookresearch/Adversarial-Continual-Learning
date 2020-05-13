4474bc02122998bb74202ac0bcc90cc0e5024f62
SaynaEbrahimi <sayna@berkeley.edu>
2020-05-13 01:54:29 -0700

    adding facebook header

--- data/split_miniimagenet.py
+++ data/split_miniimagenet.py
@@ -1,3 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 import pickle
 import numpy as np
 import os
